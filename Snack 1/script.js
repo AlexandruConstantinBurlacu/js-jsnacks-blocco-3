2476738fb906a80062308e268523fa11ea349987
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-12 17:16:53 +0100

    primo snack

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -1,8 +1,21 @@
 // Scrivi una funzione che accetti una stringa come 
 // argomento e la restituisca girata (es. "Ciao" -> "oaiC")
 
+const wordElement = document.querySelector('#parolaStringa');
+const invertedElement = document.querySelector('#parolaInvertita');
 
 // funzione pulsante
 document.querySelector('#buttonReverse').onclick = function() {
-    
+
+    const wordString = wordElement.value;
+    const reversedString = reverse(wordString);
+
+    invertedElement.innerText = reversedString;
 }
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+
+
