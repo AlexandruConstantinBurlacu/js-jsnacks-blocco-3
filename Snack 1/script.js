44db826ae1904be8a1e2e5f84c375fc43814288c
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-12 16:57:51 +0100

    first commit

---
+++ Snack 1/script.js
@@ -0,0 +1,8 @@
+// Scrivi una funzione che accetti una stringa come 
+// argomento e la restituisca girata (es. "Ciao" -> "oaiC")
+
+
+// funzione pulsante
+document.querySelector('#buttonReverse').onclick = function() {
+    
+}
