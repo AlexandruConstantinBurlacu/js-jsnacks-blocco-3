7b356585bb5afcbf3a5c8e6a2d14bf396de61456
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-12 17:33:42 +0100

    dichiaro 2 array

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -2,3 +2,8 @@
 // (date per scontato che abbiano lo stesso numero di elementi) 
 // prendendo alternativamente gli elementi da uno e dall’altro
 // es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const primoArray = ['a', 'b', 'c', 'd', 'e', 'f'];
+const secondoArray = [1, 2, 3, 4, 5, 6];
+
+
